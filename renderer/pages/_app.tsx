d7bb4acad66b122ecc34971982e425d1d3a847f3
miyabi-satoh <miyabi.satoh@gmail.com>
2022-04-15 00:52:53 +0900

    ログ表示

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
+
+// useRouter().query の undefine 対策
+MyApp.getInitialProps = async () => ({ pageProps: {} });
 
 export default MyApp;
--- renderer/pages/view/[log].tsx
+++ renderer/pages/view/[log].tsx
@@ -1,15 +1,45 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Container, Stack } from "react-bootstrap";
+import { FaChevronLeft } from "react-icons/fa";
 import Layout from "../../components/Layout";
 
 const ViewPage = () => {
   const router = useRouter();
   const { log } = router.query;
 
+  const [logContents, setLogContents] = useState<string>("");
+
+  const handleBack = () => {
+    window.location.href = "/";
+  };
+
+  const readLog = async () => {
+    const path = await window.api.getResourcePath(`cli/${log}.log`);
+    const res = await window.api.readFile(path);
+    // Save empty webhook triggers
+    const logs = res.status ? res.stderr : res.stdout;
+    console.log(path, res);
+
+    setLogContents(logs);
+  };
+
+  useEffect(() => {
+    readLog();
+  }, []);
+
   return (
     <Layout>
-      <div className="p-4">
-        <div>ViewPage:{log}</div>
+      <div className="d-flex flex-column vh-100">
+        <div className="bg-secondary text-white p-3">
+          <Stack direction="horizontal" gap={3}>
+            <Button variant="info" className="text-white" onClick={handleBack}>
+              <FaChevronLeft /> Back
+            </Button>
+            <h4 className="mb-0">{log}.log</h4>
+          </Stack>
+        </div>
+        <pre className="flex-grow-1 p-3 text-body">{logContents}</pre>
       </div>
     </Layout>
   );
